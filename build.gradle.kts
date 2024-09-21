acd54a6c1fba22f7a8758df2cd292654b12121f7
Alen Mujezinovic <76110+flashingpumpkin@users.noreply.github.com>
2024-09-22 00:23:12 +0100

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version libs.versions.kotlin
+    id("maven-publish")
 }
 
 group = "io.effectivelabs"
@@ -10,10 +11,7 @@ repositories {
 }
 
 dependencies {
-
-
     implementation("jakarta.inject:jakarta.inject-api:2.0.1")
-
     testImplementation(kotlin("test"))
     testImplementation(libs.mockito)
     testImplementation(libs.mockito.core)
@@ -26,4 +24,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl("https://maven.pkg.github.com/flashingpumpkin/kotlin-injector")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
